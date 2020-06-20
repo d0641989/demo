24cc4007823675ab12117d1a303fe8c2e9d29237
d0641989 <43879364+d0641989@users.noreply.github.com>
2020-06-20 14:11:58 +0800

    1.7

--- src/test/java/IDnumberTest.java
+++ src/test/java/IDnumberTest.java
@@ -1,7 +1,13 @@
 package hw0620;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class IDnumberTest{
 	@Test
 	public void testID1() {
